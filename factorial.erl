4b9bbf0f7d31d9e780d71ca32ad328815a2aab05
shaman <esh.eburg@gmail.com>
2019-09-04 19:32:47 +0300

    Update factorial.erl

--- factorial.erl
+++ factorial.erl
@@ -1,10 +1,8 @@
 -module(factorial).
 -export([my_fac/1]).
 
-my_fac(0) -> 1;
-my_fac(1) -> 1;
 my_fac(N) -> my_fac(N, 1).
 
 my_fac(N, Acc) when N > 1 ->
 	my_fac(N - 1, N * Acc);
-my_fac(1, Acc) -> Acc.
+my_fac(_, Acc) -> Acc.
